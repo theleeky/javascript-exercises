6f67967c76e6f72d8fec1fd02c4433847a83cb45
Malek <malekbusari@gmail.com>
2022-08-24 19:10:00 +0100

    ex3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    let newString = "";
+    for (let i = 0; i < array.length; i++) {
+        newString += array[(array.length - i)-1];
+    }
+    return newString
 };
 
 // Do not edit below this line
