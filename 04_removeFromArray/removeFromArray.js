af412912b9266b439d23d875bb86f0e70b001f92
Malek <malekbusari@gmail.com>
2022-08-24 20:06:42 +0100

    ex4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...num) {
+    for (let i = 0; i < num.length; i ++) {
+        if (array.indexOf(num[i]) === -1){
+            continue;
+        } else {
+            array.splice(array.indexOf(num[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// loop through numbers
+// check if number is in the array
+// if 
