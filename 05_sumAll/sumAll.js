4f2281efbd6c0943b0bfbc5e90de744e6bdb0ebe
Malek <malekbusari@gmail.com>
2022-08-24 20:25:44 +0100

    ex5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != 'number' || typeof num2 != 'number'){
+        return "ERROR"
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } 
 
-};
+    const lower = (num1 > num2) ? num2 : num1;
+    const upper = (num1 > num2) ? num1 : num2;
+    let final = 0;
+    
+    for (let i = lower; i <= upper; i++) {
+        final += i;
+    }
+
+    return final
+}
 
 // Do not edit below this line
 module.exports = sumAll;
