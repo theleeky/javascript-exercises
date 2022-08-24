9d2e31b22f691233c636b9dcd662a79f6a8a3db0
Malek <malekbusari@gmail.com>
2022-08-24 18:59:41 +0100

    e2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        return word.repeat(num)
+    }
 };
 
 // Do not edit below this line
