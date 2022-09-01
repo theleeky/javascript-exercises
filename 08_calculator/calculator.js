f24e71d6d91f42dce9ffd675056470ca3967d2e6
Malek <malekbusari@gmail.com>
2022-09-01 23:02:37 +0100

    ex8 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let c = 0;
+	return array.reduce((a,b) => a+b, c)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let c = 1;
+	return array.reduce((a,b) => a*b, c)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num){
+  return factor(num);
+}
+
+function factor(num){
+  if (num === 0){
+    return 1;
+  } else {
+    return factor(num-1)*num;
+  }
+}
 
 // Do not edit below this line
 module.exports = {
