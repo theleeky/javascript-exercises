7d676a5645d61266512f87cac66f80281e25d3b0
Malek <malekbusari@gmail.com>
2022-09-02 14:21:54 +0100

    ex10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 1) {
+        return "OOPS"
+    }
+    if (num == 1 || num == 2){
+        return 1; 
+    } else {
+        return fibonacci (num-1) + fibonacci (num-2)
+    }
 };
 
 // Do not edit below this line
