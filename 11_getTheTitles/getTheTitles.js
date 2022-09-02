c415a0226f4e5a393ad58a0f836bded41f28d035
Malek <malekbusari@gmail.com>
2022-09-02 14:27:38 +0100

    ex11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let output = [];
+    for (let i = 0; i < array.length; i++){
+        output[i] = array[i].title;
+    }
+    return output;
 };
 
 // Do not edit below this line
